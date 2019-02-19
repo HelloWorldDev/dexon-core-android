d537b3db99a06c4801f9909e03a9fc437a03aa5a
Kihon Liu <kihon@cobinhood.com>
2019-02-19 17:31:27 +0800

    Add `Token`

---
+++ dekusan_core/src/main/java/org/dexon/dekusan/core/model/Token.kt
@@ -0,0 +1,10 @@
+package org.dexon.dekusan.core.model
+
+data class Token(
+    val symbol: String,
+    val address: Address,
+    val chain: ChainDefinition,
+    val decimals: Int = 18,
+    val type: String? = null,
+    val name: String? = null
+)
